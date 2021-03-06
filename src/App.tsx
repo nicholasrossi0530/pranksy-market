564a699b231b13f54daadc6783075208a6ede228
nicholasrossi0530 <nrossi0530@gmail.com>
2021-03-06 01:45:32 -0800

    [GQL] Adding Apollo client and Jan box GQL query.

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,80 @@
-import React from 'react';
-import './App.css';
-import { Grid } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import MarketCard from './MarketCard';
-import ComingSoonCard from './ComingSoonCard';
-import ArtCards from './ArtCards';
+import React from "react";
+import "./App.css";
+import { Grid } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import MarketCard from "./MarketCard";
+import ComingSoonCard from "./ComingSoonCard";
+import ArtCards from "./ArtCards";
+import { useQuery, gql } from "@apollo/client";
+
+const EXCHANGE_RATES = gql`
+{
+  transactions(orderBy: "timestamp", orderDirection: "desc", where: {value_gt: 0}) {
+    id
+    hash
+    index
+    from
+    to
+    value
+    gasUsed
+    gasPrice
+    timestamp
+    tokenId
+  }
+}
+
+`;
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
-    justifyContent: 'space-evenly',
-    marginTop: '50px',
-    flexWrap: 'wrap',
-    padding: '0px 150px'
-  }
+    display: "flex",
+    justifyContent: "space-evenly",
+    marginTop: "50px",
+    flexWrap: "wrap",
+    padding: "0px 150px",
+  },
 }));
 
-function App() {
+interface ITransaction {
+  from: string;
+  gasPrice: string;
+  gasUsed: string;
+  hash: string;
+  id: string;
+  index: string;
+  timestamp: string;
+  to: string;
+  tokenId: string;
+  value: string;
+}
 
+function App() {
+  const { loading, error, data } = useQuery(EXCHANGE_RATES);
   const classes = useStyles();
   const JAN_BOX_ADDRESS = "0x5F8061F9d6A2Bb4688F46491cCA7658e214E2Cb6";
   const FEB_BOX_ADDRESS = "0x067ab2FbdBED63401aF802d1DD786E6D83b0ff1B";
   const ART_ADDRESS = "0x6d4530149e5B4483d2F7E60449C02570531A0751";
 
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :( {JSON.stringify(error)}</p>;
+
   return (
     <Grid container spacing={1}>
       <Grid container item xs={12} spacing={4} className={classes.root}>
-        <MarketCard
+        {data.transactions.length}
+        {data.transactions.map((transaction: ITransaction) => {
+        const transactionDate = new Date(parseInt(transaction.timestamp)*1000);
+        return (
+          <div>
+            <p>{parseInt(transaction.value)/Math.pow(10,18)} Eth</p>
+            <p>Token {transaction.tokenId}</p>
+            <p>{transactionDate.toString()}</p>
+            <br />
+            <br />
+            <br />
+          </div>
+        )})}
+        {/* <MarketCard
           address={JAN_BOX_ADDRESS}
         />
         <MarketCard
@@ -35,7 +83,7 @@ function App() {
         <ComingSoonCard />
         <ArtCards 
           address={ART_ADDRESS}
-        />
+        /> */}
       </Grid>
     </Grid>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'https://api.thegraph.com/subgraphs/name/nicholasrossi0530/nft-box-graph',
+  cache: new InMemoryCache()
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+     <ApolloProvider client={client}>
+        <App />
+     </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
